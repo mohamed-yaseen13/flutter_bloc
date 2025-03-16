7a11c3dd59d81fb1a1042e8348525fb10256c5ad
mohamed-yaseen <01221316865m@gmail.com>
2025-03-16 07:47:20 +0200

    bloc provider for home screen

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_flutter_bloc/business_logic/cubit/characters_cubit.dart';
+import 'package:my_flutter_bloc/data/api/api_service.dart';
+import 'package:my_flutter_bloc/data/repo/characters_repository.dart';
 import 'package:my_flutter_bloc/presentation/screens/character_details_screen.dart';
 import 'package:my_flutter_bloc/presentation/screens/characters_screen.dart';
 import 'package:my_flutter_bloc/routes/app_routes.dart';
 
 class AppRouter {
-  static Route<dynamic>? onGenerateRoute(
-    RouteSettings settings,
-  ) {
+  static late CharactersRepository charactersRepository;
+
+  static late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(ApiService());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.home:
         return MaterialPageRoute(
-          builder: (_) => CharactersScreen(),
+          builder:
+              (_) => BlocProvider(
+                create: (BuildContext context) => charactersCubit,
+                child: CharactersScreen(),
+              ),
         );
       case AppRoutes.details:
-        return MaterialPageRoute(
-          builder: (_) => CharacterDetailsScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
     }
     return null;
   }
